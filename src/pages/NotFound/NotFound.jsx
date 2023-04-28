44ef3b2d8c860510ff0fc6fd19f9ce0606d9a96b
Sol <solzito.1889@gmail.com>
2023-04-28 19:09:24 -0300

    pag para los 404

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        padding: "13px",
+      }}
+    >
+      <h1>
+        <strong>404 </strong>Pages not found
+      </h1>
+      <p>The request url was not found on this server</p>
+    </div>
+  );
+};
+
+export default NotFound;
